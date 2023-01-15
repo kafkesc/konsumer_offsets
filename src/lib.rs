e9db96b4b76b00c2eecf180ab678afcf2e255299
Ivan De Marino <ivan.de.marino@gmail.com>
2023-01-15 18:23:36 +0000

    Documented `lib.rs`, serving as entrance to the crate on docs.rs

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,116 @@
-//! TODO more package doc
+//! A library to parse the content of the [Kafka] [`__consumer_offsets`] internal topic.
 //!
-//! # Parsing Kafka _entrails_
+//! What this crate does, and what data it can parse, will make
+//! sense only if you have read about how Kafka tracks Consumer Offsets. Especially, what
+//! part the Consumer Group Coordinator(s) play in that. A _great_ resource to learn (or refresh)
+//! how the [Consumer Group Protocol] works, can be found on the [Confluent Developer] website.
+//!
+//! Also, as it will be referenced when documenting the data itself, it is important to know
+//! that [`__consumer_offsets`] uses [Log Compaction]: this means that it follows all the compaction
+//! rules, including the handling of _tombstone_ messages, used to "mark for deletion" all messages
+//! of the given key.
+//!
+//! # Example
+//!
+//! Here is how the code should be structured to handle the flow of messages coming out of
+//! the [`__consumer_offsets`] topic.
+//!
+//! ```
+//! use konsumer_offsets::{KonsumerOffsetsData, KonsumerOffsetsError};
+//!
+//! fn consume_consumer_offsets_message(k_bytes: Option<Vec<u8>>, p_bytes: Option<Vec<u8>>) {//!
+//!     match KonsumerOffsetsData::try_from_bytes_vec(k_bytes, p_bytes) {
+//!         Ok(kod) => {
+//!             match kod {
+//!                 KonsumerOffsetsData::OffsetCommit(offset_commit) => {
+//!                     /* ... a consumer committed an offset for a partition in a topic ... */
+//!                 }
+//!                 KonsumerOffsetsData::GroupMetadata(group_metadata) => {
+//!                     /* ... a consumer joined or leaved the group ... */
+//!                 }
+//!             }
+//!         }
+//!         Err(e) => {
+//!             /* ... handle parsing error ... */
+//!         }
+//!     }
+//! }
+//! ```
+//!
+//! # 1 topic, 2 different data types
+//!
+//! The messages (a.k.a. records) in [`__consumer_offsets`] are encoded using a _bespoke_ binary
+//! protocol, and each message can contain 1 of 2 possible data types (at least as per Kafka 3):
+//!
+//! * [`OffsetCommit`]
+//! * [`GroupMetadata`]
+//!
+//! Which data type is contained in a message is determined by parsing the first 2-bytes integer,
+//! carrying a `message_version` code: this will be amongst the fields of both structs.
+//!
+//!
+//! ## [`OffsetCommit`] a.k.a. "where is the consumer at?"
+//!
+//! The Kafka uses code generation to materialise [`OffsetCommit`] into Java code, and this is
+//! actually composed of 2 json definitions, that at compile time get turned into Java Classes:
+//! [`OffsetCommitKey`] and [`OffsetCommitValue`].
+//!
+//! Assuming you are familiar with the [Consumer Group Protocol] (as discussed above): this
+//! is what the Group Coordinator stores into the [`__consumer_offsets`] topic, when a consumer,
+//! assignor of a specific _partition_, sends a request to the coordinator to _commit_ the _offset_
+//! up to which it has consumed. It's _essentially_ informing the group coordinator that:
+//!
+//! > "I consumed this partition for this topic up to this offset".
+//!
+//! The information stored in [`OffsetCommit`] can be used to track
+//! _who has consumed what until now_. Refer to its own documentation for further details.
+//!
+//! ## [`GroupMetadata`] a.k.a. "what are the members of this group?"
+//!
+//! Similarly to [`OffsetCommit`], Kafka uses code generation to materialise [`GroupMetadata`]
+//! into Java code, and this is actually composed of 2 json definitions, that at compile time
+//! get turned into Java Classes: [`GroupMetadataKey`] and [`GroupMetadataValue`].
+//!
+//! [`GroupMetadata`] contains the current state of a consumer group, and it used by the Group
+//! Coordinator to track "which consumer is subscribed to what topic" and "which consumer
+//! is assigned of which partition". The metadata are divided into 2 classes:
+//!
+//! Membership metadata:
+//!
+//! 1. Members registered in this group
+//! 2. Current protocol assigned to the group (e.g. partition assignment strategy for consumers)
+//! 3. Protocol metadata associated with group members
+//!
+//! State metadata:
+//!
+//! 1. consumer group state
+//! 2. generation ID
+//! 3. leader ID
+//!
+//! Compared to [`OffsetCommit`], [`GroupMetadata`] appears _relatively infrequently_ in
+//! [`__consumer_offsets`]: this is because it's usually produced when consumers join or leave
+//! groups.
+//!
+//! ## [`KonsumerOffsetsData`] i.e. "making it rusty"
+//!
+//! The entry point to this crate is [`KonsumerOffsetsData`]: this is an enum where each variant
+//! wraps one of the 2 possible data types seen above, as they are parsed from bytes coming
+//! out of [`__consumer_offsets`].
+//!
+//! For convenience we provide 2 `try_from_*` factory methods:
+//!
+//! * [`KonsumerOffsetsData::try_from_bytes`]
+//! * [`KonsumerOffsetsData::try_from_bytes_vec`]
+//!
+//! This is because some Kafka clients might return the `(key,payload)` _tuple_ as `[u8]`,
+//! others as `Vec<u8>`.
+//!
+//! # A few words about parsing Kafka _entrails_
 //!
 //! Kafka runs on the JVM, so it's limited to what the JVM supports.
 //!
-//! This crate deals with these limitations, and the data it parses are a reflection
-//! of what Java can indeed support. So, for example, in places where one would expect
+//! This crate deals with these limitations, and the data it parses and the fields we use to
+//! store it, are a reflection of what Java supports. For example, in places where one would expect
 //! an unsigned integer, like a string length or an ever increasing identifier, we use
 //! the signed integers that Java supports.
 //!
@@ -61,7 +166,23 @@
 //! +-----------------------------------------------------------+
 //! ```
 //!
+//! # Disclaimer
+//!
+//! [Confluent] is a great place to start if you are looking for a commercial Kafka solution,
+//! including PaaS, SaaS, training and more. **BUT** it is completely unrelated from this
+//! crate and any reference to it is purely educational.
+//!
 //! [Primitive Types]: https://docs.oracle.com/javase/tutorial/java/nutsandbolts/datatypes.html
+//! [`__consumer_offsets`]: https://kafka.apache.org/documentation/#impl_offsettracking
+//! [Kafka]: https://kafka.apache.org/
+//! [Consumer Group Protocol]: https://developer.confluent.io/learn-kafka/architecture/consumer-group-protocol/
+//! [Confluent Developer]: https://developer.confluent.io/
+//! [Confluent]: https://www.confluent.io/about/
+//! [`OffsetCommitKey`]: https://github.com/apache/kafka/blob/trunk/core/src/main/resources/common/message/OffsetCommitKey.json
+//! [`OffsetCommitValue`]: https://github.com/apache/kafka/blob/trunk/core/src/main/resources/common/message/OffsetCommitValue.json
+//! [`GroupMetadataKey`]: https://github.com/apache/kafka/blob/trunk/core/src/main/resources/common/message/GroupMetadataKey.json
+//! [`GroupMetadataValue`]: https://github.com/apache/kafka/blob/trunk/core/src/main/resources/common/message/GroupMetadataValue.json
+//! [Log Compaction]: https://kafka.apache.org/documentation/#compaction
 
 mod errors;
 mod group_metadata;
